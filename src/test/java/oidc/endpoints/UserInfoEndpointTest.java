05f5216fec69448ce118ce9e1119be0470172bd0
Okke Harsta <oharsta@zilverline.com>
2019-04-29 13:13:45 +0200

    UserInfoEndpoint various methods

--- src/test/java/oidc/endpoints/UserInfoEndpointTest.java
+++ src/test/java/oidc/endpoints/UserInfoEndpointTest.java
@@ -8,36 +8,54 @@ import org.junit.Test;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class UserInfoEndpointTest extends AbstractIntegrationTest {
 
     @Test
     public void getUserInfo() {
         userInfo("GET");
+        userInfoWithAuthorizationHeader();
     }
 
     @Test
     public void postUserInfo() {
         userInfo("POST");
+
     }
 
     private void userInfo(String method) {
-        String code = doAuthorize("http@//mock-sp");
-        Map<String, Object> body = doToken(code);
-        String accessToken = (String) body.get("access_token");
+        String accessToken = getAccessToken();
         RequestSpecification header = given()
                 .when()
-                .header("Content-type", "application/x-www-form-urlencoded")
-                .header("Authorization", "Bearer " + accessToken);
+                .header("Content-type", "application/x-www-form-urlencoded");
 
         Response response = method.equals("POST") ? header
-                .formParam("token", accessToken)
+                .formParam("access_token", accessToken)
                 .post("oidc/userinfo") :
-                header.queryParams("token", accessToken).get("oidc/userinfo");
+                header.queryParams("access_token", accessToken).get("oidc/userinfo");
+        assertResponse(response);
+    }
+
+    private void userInfoWithAuthorizationHeader() {
+        String accessToken = getAccessToken();
+        Response response = given()
+                .when()
+                .header("Content-type", "application/x-www-form-urlencoded")
+                .header("Authorization", "Bearer " + accessToken)
+                .get("oidc/userinfo");
+        assertResponse(response);
+    }
+
+    private String getAccessToken() {
+        String code = doAuthorize("http@//mock-sp");
+        Map<String, Object> body = doToken(code);
+        return (String) body.get("access_token");
+    }
+
+    private void assertResponse(Response response) {
         Map<String, Object> result = response.as(mapTypeRef);
         assertEquals("john.doe@example.org", result.get("email"));
         assertEquals(true, result.containsKey("sub"));
-
     }
 }
