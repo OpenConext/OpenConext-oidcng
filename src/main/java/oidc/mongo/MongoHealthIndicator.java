74e4a28a6ef96606e8a1b30ca4193f3ea8eafa8f
Okke Harsta <oharsta@zilverline.com>
2019-05-12 09:19:00 +0200

    Do no use spring test in non-tests

--- src/main/java/oidc/mongo/MongoHealthIndicator.java
+++ src/main/java/oidc/mongo/MongoHealthIndicator.java
@@ -1,17 +1,14 @@
 package oidc.mongo;
 
-import com.mongodb.MongoClient;
-import com.mongodb.ReplicaSetStatus;
-import com.mongodb.client.internal.MongoClientDelegate;
-import com.mongodb.connection.ClusterDescription;
-import com.mongodb.connection.ClusterType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 import org.springframework.stereotype.Component;
-import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Component
 public class MongoHealthIndicator implements HealthIndicator {
@@ -26,13 +23,9 @@ public class MongoHealthIndicator implements HealthIndicator {
     @Override
     public Health health() {
         SimpleMongoDbFactory mongoDbFactory = (SimpleMongoDbFactory) mongoTemplate.getMongoDbFactory();
-        MongoClient mongoClient = (MongoClient) ReflectionTestUtils.getField(mongoDbFactory, "mongoClient");
-        ReplicaSetStatus replicaSetStatus = mongoClient.getReplicaSetStatus();
-        MongoClientDelegate delegate = (MongoClientDelegate) ReflectionTestUtils.getField(mongoClient, "delegate");
-        ClusterDescription clusterDescription= delegate.getCluster().getCurrentDescription();
+        String collections = StreamSupport.stream(mongoTemplate.getDb().listCollectionNames().spliterator(), false).collect(Collectors.joining(", "));
         return Health.up()
-                .withDetail("Cluster", clusterDescription)
-                .withDetail("ReplicaSetStatus", replicaSetStatus != null ? replicaSetStatus : "No replica status")
+                .withDetail("Collections", collections)
                 .build();
     }
 }
