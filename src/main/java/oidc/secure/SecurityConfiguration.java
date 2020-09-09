bce5a5b179dd617c06620225131849fb4c2ea096
Okke Harsta <oharsta@zilverline.com>
2020-09-09 09:43:15 +0200

    WIP for investigation if migration to spring-security-saml2-service-provider is possible

--- src/main/java/oidc/secure/SecurityConfiguration.java
+++ src/main/java/oidc/secure/SecurityConfiguration.java
@@ -19,27 +19,24 @@ package oidc.secure;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
-import oidc.config.AppConfig;
-import oidc.config.BeanConfig;
 import oidc.config.OidcCorsConfigurationSource;
 import oidc.config.TokenUsers;
-import oidc.crypto.KeyGenerator;
 import oidc.log.MDCContextFilter;
 import oidc.repository.UserRepository;
-import oidc.web.ConfigurableSamlAuthenticationRequestFilter;
 import oidc.web.FakeSamlAuthenticationFilter;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.core.env.Environment;
 import org.springframework.core.env.Profiles;
 import org.springframework.core.io.Resource;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configurers.provisioning.InMemoryUserDetailsManagerConfigurer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -47,19 +44,17 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.saml.key.SimpleKey;
-import org.springframework.security.saml.provider.config.RotatingKeys;
-import org.springframework.security.saml.provider.service.config.ExternalIdentityProviderConfiguration;
-import org.springframework.security.saml.provider.service.config.SamlServiceProviderSecurityConfiguration;
-import org.springframework.security.saml.provider.service.config.SamlServiceProviderSecurityDsl;
+import org.springframework.security.saml2.credentials.Saml2X509Credential;
+import org.springframework.security.saml2.provider.service.registration.InMemoryRelyingPartyRegistrationRepository;
+import org.springframework.security.saml2.provider.service.registration.RelyingPartyRegistration;
+import org.springframework.security.saml2.provider.service.registration.RelyingPartyRegistrationRepository;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 import java.nio.charset.Charset;
-import java.util.Collections;
-import java.util.stream.Stream;
 
-import static org.springframework.security.saml.provider.service.config.SamlServiceProviderSecurityDsl.serviceProvider;
+import static org.springframework.security.config.Customizer.withDefaults;
 
+@EnableScheduling
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration {
@@ -68,32 +63,28 @@ public class SecurityConfiguration {
 
     @Configuration
     @Order(1)
-    public static class SamlSecurity extends SamlServiceProviderSecurityConfiguration {
+    public static class SamlSecurity extends WebSecurityConfigurerAdapter {
 
         private String idpAlias;
         private String[] idpMetaDataUrls;
         private String idpNameId;
         private Resource metadataSigningCertificatePath;
         private Environment environment;
-        private AppConfig appConfiguration;
         private ObjectMapper objectMapper;
         private UserRepository userRepository;
         private Resource privateKeyPath;
         private Resource certificatePath;
 
-        public SamlSecurity(BeanConfig beanConfig,
-                            @Qualifier("appConfig") AppConfig appConfig,
-                            Environment environment,
-                            ObjectMapper objectMapper,
-                            UserRepository userRepository,
-                            @Value("${private_key_path}") Resource privateKeyPath,
-                            @Value("${certificate_path}") Resource certificatePath,
-                            @Value("${idp.alias}") String idpAlias,
-                            @Value("${idp.metadata_urls}") String[] idpMetaDataUrls,
-                            @Value("${idp.name_id}") String idpNameId,
-                            @Value("${idp.metadata_signing_certificate_path}") Resource metadataSigningCertificatePath) {
-            super("oidc", beanConfig);
-            this.appConfiguration = appConfig;
+        public SamlSecurity(
+                Environment environment,
+                ObjectMapper objectMapper,
+                UserRepository userRepository,
+                @Value("${private_key_path}") Resource privateKeyPath,
+                @Value("${certificate_path}") Resource certificatePath,
+                @Value("${idp.alias}") String idpAlias,
+                @Value("${idp.metadata_urls}") String[] idpMetaDataUrls,
+                @Value("${idp.name_id}") String idpNameId,
+                @Value("${idp.metadata_signing_certificate_path}") Resource metadataSigningCertificatePath) {
             this.environment = environment;
             this.objectMapper = objectMapper;
             this.userRepository = userRepository;
@@ -105,58 +96,75 @@ public class SecurityConfiguration {
             this.metadataSigningCertificatePath = metadataSigningCertificatePath;
         }
 
+        @Bean
+        public RelyingPartyRegistrationRepository relyingPartyRegistrationRepository() {
+
+            //remote WebSSO Endpoint - Where to Send AuthNRequests to
+            String webSsoEndpoint = "ttps://engine.test2.surfconext.nl/authentication/idp/single-sign-on";
+            //local registration ID
+            String registrationId = "oidcng";
+            //local entity ID - autogenerated based on URL
+            String localEntityIdTemplate = "{baseUrl}/saml2/service-provider-metadata/{registrationId}";
+            //local SSO URL - autogenerated, endpoint to receive SAML Response objects
+            String acsUrlTemplate = "{baseUrl}/login/saml2/sso/{registrationId}";
+            //local signing (and local decryption key and remote encryption certificate)
+            Saml2X509Credential signingCredential = getSigningCredential();
+            //IDP certificate for verification of incoming messages
+            Saml2X509Credential idpVerificationCertificate = getVerificationCertificate();
+            RelyingPartyRegistration rp = RelyingPartyRegistration
+                    .withRegistrationId(registrationId)
+                    .providerDetails(config -> config
+                            .entityId(idpNameId)
+                            .webSsoUrl(webSsoEndpoint))
+                    .credentials(c -> c.add(signingCredential))
+                    .credentials(c -> c.add(idpVerificationCertificate))
+                    .localEntityIdTemplate(localEntityIdTemplate)
+                    .assertionConsumerServiceUrlTemplate(acsUrlTemplate)
+                    .build();
+
+            return new InMemoryRelyingPartyRegistrationRepository(rp);
+        }
+
         @Override
         protected void configure(HttpSecurity http) throws Exception {
-            super.configure(http);
             http.cors().configurationSource(new OidcCorsConfigurationSource()).configure(http);
-
-            SamlServiceProviderSecurityDsl samlServiceProviderSecurityDsl = http.apply(serviceProvider());
-            samlServiceProviderSecurityDsl
-                    .configure(appConfiguration)
-                    .rotatingKeys(getKeys());
-            Stream.of(this.idpMetaDataUrls).map(String::trim).forEach(idpMetaDataUrl -> {
-                ExternalIdentityProviderConfiguration idp = new ExternalIdentityProviderConfiguration();
-                idp.setAssertionConsumerServiceIndex(0)
-                        .setNameId(idpNameId)
-                        .setMetadataTrustCheck(true)
-                        .setAlias(idpAlias)
-                        .setVerificationKeys(Collections.singletonList(strip(read(metadataSigningCertificatePath))))
-                        .setMetadata(idpMetaDataUrl);
-                samlServiceProviderSecurityDsl.identityProvider(idp);
-            });
+            http
+                    .authorizeRequests(authorize -> authorize
+                            .antMatchers("oidc/authorize").authenticated()
+                    ).saml2Login(withDefaults());
             http.addFilterBefore(new MDCContextFilter(), BasicAuthenticationFilter.class);
 
             if (environment.acceptsProfiles(Profiles.of("dev"))) {
                 http.addFilterBefore(new FakeSamlAuthenticationFilter(userRepository, objectMapper),
-                        ConfigurableSamlAuthenticationRequestFilter.class);
+                        BasicAuthenticationFilter.class);
             }
         }
 
-
-
-        private RotatingKeys getKeys() throws Exception {
-            String privateKey;
-            String certificate;
-            if (this.privateKeyPath.exists() && this.certificatePath.exists()) {
-                privateKey = read(this.privateKeyPath);
-                certificate = read(this.certificatePath);
-            } else {
-                LOG.info("Generating public / private key pair for SAML trusted proxy");
-                String[] keys = KeyGenerator.generateKeys();
-                privateKey = keys[0];
-                certificate = keys[1];
-            }
-            return new RotatingKeys()
-                    .setActive(
-                            new SimpleKey()
-                                    .setName("sp-signing-key")
-                                    .setPrivateKey(privateKey)
-                                    //to prevent null-pointer in SamlKeyStoreProvider
-                                    .setPassphrase("")
-                                    .setCertificate(certificate)
-                    );
-        }
-
+        //
+//
+//        private RotatingKeys getKeys() throws Exception {
+//            String privateKey;
+//            String certificate;
+//            if (this.privateKeyPath.exists() && this.certificatePath.exists()) {
+//                privateKey = read(this.privateKeyPath);
+//                certificate = read(this.certificatePath);
+//            } else {
+//                LOG.info("Generating public / private key pair for SAML trusted proxy");
+//                String[] keys = KeyGenerator.generateKeys();
+//                privateKey = keys[0];
+//                certificate = keys[1];
+//            }
+//            return new RotatingKeys()
+//                    .setActive(
+//                            new SimpleKey()
+//                                    .setName("sp-signing-key")
+//                                    .setPrivateKey(privateKey)
+//                                    //to prevent null-pointer in SamlKeyStoreProvider
+//                                    .setPassphrase("")
+//                                    .setCertificate(certificate)
+//                    );
+//        }
+//
         @SneakyThrows
         private String read(Resource resource) {
             LOG.info("Reading resource: " + resource.getFilename());
