39e005f2e7bc07030a7c18adb3c2453bebf3c44e
Okke Harsta <oharsta@zilverline.com>
2020-10-16 15:56:50 +0200

    Refactored test and no cookies supported feature

--- src/main/java/oidc/web/ConcurrentSavedRequestAwareAuthenticationSuccessHandler.java
+++ src/main/java/oidc/web/ConcurrentSavedRequestAwareAuthenticationSuccessHandler.java
@@ -2,14 +2,10 @@ package oidc.web;
 
 import oidc.model.AuthenticationRequest;
 import oidc.repository.AuthenticationRequestRepository;
-import oidc.secure.KeyRollover;
 import oidc.user.OidcSamlAuthentication;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -26,14 +22,9 @@ public class ConcurrentSavedRequestAwareAuthenticationSuccessHandler extends Sim
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
             throws IOException {
         OidcSamlAuthentication samlAuthentication = (OidcSamlAuthentication) authentication;
-        AuthenticationRequest authenticationRequest = authenticationRequestRepository.findById(samlAuthentication.getAuthenticationRequestID()).orElseThrow(
-                () -> new IllegalArgumentException("No Authentication Request found for ID: " + samlAuthentication.getAuthenticationRequestID()));
+        AuthenticationRequest authenticationRequest = authenticationRequestRepository.findById(samlAuthentication.getAuthenticationRequestID())
+                .orElseThrow(() -> new IllegalArgumentException("No Authentication Request found for ID: " + samlAuthentication.getAuthenticationRequestID()));
         String originalRequestUrl = authenticationRequest.getOriginalRequestUrl();
-
-        String append = originalRequestUrl.contains("?") ? "&" : "?";
-        //To check if cookies are blocked
-//        originalRequestUrl += (append + AUTHENTICATION_SUCCESS_QUERY_PARAMETER + "=" + true);
-        //Redirect to authorize endpoint
         getRedirectStrategy().sendRedirect(request, response, originalRequestUrl);
     }
 }
--- src/test/java/oidc/web/ConcurrentSavedRequestAwareAuthenticationSuccessHandlerTest.java
+++ src/test/java/oidc/web/ConcurrentSavedRequestAwareAuthenticationSuccessHandlerTest.java
@@ -8,45 +8,35 @@ import org.junit.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Date;
 import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.isNull;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class ConcurrentSavedRequestAwareAuthenticationSuccessHandlerTest implements TestUtils {
 
-    private AuthenticationRequestRepository authenticationRequestRepository = mock(AuthenticationRequestRepository.class);
+    @Test
+    public void onAuthenticationSuccess() throws IOException {
+        AuthenticationRequestRepository authenticationRequestRepository = mock(AuthenticationRequestRepository.class);
 
-    private ConcurrentSavedRequestAwareAuthenticationSuccessHandler subject =
-            new ConcurrentSavedRequestAwareAuthenticationSuccessHandler(authenticationRequestRepository);
+        ConcurrentSavedRequestAwareAuthenticationSuccessHandler subject =
+                new ConcurrentSavedRequestAwareAuthenticationSuccessHandler(authenticationRequestRepository);
 
-//    @Test
-//    public void onAuthenticationSuccess() throws IOException {
-//        MockHttpServletResponse response = new MockHttpServletResponse();
-//        when(authenticationRequestRepository.findById("ID"))
-//                .thenReturn(Optional.of(new AuthenticationRequest("ID", new Date(), "http://localhost")));
-//        subject.onAuthenticationSuccess(new MockHttpServletRequest(), response, new OidcSamlAuthentication(FakeSamlAuthenticationFilter.getAssertion(),
-//               FakeSamlAuthenticationFilter.getUser(objectMapper, new MockHttpServletRequest()),
-//                "ID"));
-//        assertEquals("http://localhost?authentication_success=true", response.getHeader("Location"));
-//        assertEquals(HttpServletResponse.SC_MOVED_TEMPORARILY, response.getStatus());
-//    }
-//
-//    @Test
-//    public void onAuthenticationSuccessFailure() throws IOException {
-//        MockHttpServletResponse response = new MockHttpServletResponse();
-//        when(authenticationRequestRepository.findById("ID"))
-//                .thenReturn(Optional.empty());
-//        subject.onAuthenticationSuccess(new MockHttpServletRequest(), response, new OidcSamlAuthentication(FakeSamlAuthenticationFilter.getAssertion(),
-//                FakeSamlAuthenticationFilter.getUser(objectMapper, new MockHttpServletRequest()),
-//                "ID"));
-//        String redirectedUrl = response.getRedirectedUrl();
-//        assertEquals("/feedback/no-session", redirectedUrl);
-//    }
+        when(authenticationRequestRepository.findById(isNull()))
+                .thenReturn(Optional.of(new AuthenticationRequest("ID", new Date(), "client_id", "http://localhost")));
+
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        subject.onAuthenticationSuccess(new MockHttpServletRequest(), response, new OidcSamlAuthentication());
+
+        assertEquals("http://localhost", response.getHeader("Location"));
+        assertEquals(HttpServletResponse.SC_MOVED_TEMPORARILY, response.getStatus());
+    }
 
 }
