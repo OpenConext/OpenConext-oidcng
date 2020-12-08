033c697d8873afe2b9c0d9e9ead46a53a46eda79
Okke Harsta <oharsta@zilverline.com>
2020-12-08 10:21:56 +0100

    Return correct errors when using the refresh token of another client

---
+++ src/main/java/oidc/exceptions/InvalidClientException.java
@@ -0,0 +1,18 @@
+package oidc.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class InvalidClientException extends BaseException {
+    public InvalidClientException(String message) {
+        super(message);
+    }
+
+    @Override
+    public String getErrorCode() {
+        return "invalid_grant";
+    }
+
+
+}
