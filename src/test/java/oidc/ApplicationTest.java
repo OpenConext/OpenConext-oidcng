76ccff37cf69ca159d295a4d8a7fa6905a873ee1
Okke Harsta <oharsta@zilverline.com>
2022-02-08 13:46:43 +0100

    Management endpoints on internal

--- src/test/java/oidc/ApplicationTest.java
+++ src/test/java/oidc/ApplicationTest.java
@@ -2,6 +2,8 @@ package oidc;
 
 import org.junit.Test;
 
+import java.util.Map;
+
 import static io.restassured.RestAssured.given;
 import static org.apache.http.HttpStatus.SC_OK;
 import static org.hamcrest.Matchers.equalTo;
@@ -9,13 +11,20 @@ import static org.hamcrest.Matchers.equalTo;
 public class ApplicationTest extends AbstractIntegrationTest {
 
     @Test
-    public void health() throws Exception {
+    public void health() {
         given()
                 .when()
-                .get("actuator/health")
+                .get("internal/health")
                 .then()
                 .statusCode(SC_OK)
                 .body("status", equalTo("UP"));
     }
 
+    @Test
+    public void info() {
+        given()
+                .when()
+                .get("internal/info")
+                .as(Map.class);
+    }
 }
