69ebcca422f0c77602d1b81f1c218565acf78f8e
Okke Harsta <oharsta@zilverline.com>
2022-10-28 11:38:33 +0200

    Log request / response cookies

--- src/main/java/oidc/config/SessionConfig.java
+++ src/main/java/oidc/config/SessionConfig.java
@@ -13,6 +13,7 @@ import org.springframework.session.web.http.DefaultCookieSerializer;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -21,7 +22,7 @@ import java.util.stream.Stream;
 @EnableMongoHttpSession
 public class SessionConfig extends AbstractHttpSessionApplicationInitializer {
 
-    private static final Log LOG = LogFactory.getLog(CookieSerializer.class);
+    private static final Log LOG = LogFactory.getLog(SessionConfig.class);
 
     @Bean
     CookieSerializer cookieSerializer(@Value("${secure_cookie}") boolean secureCookie) {
@@ -30,19 +31,20 @@ public class SessionConfig extends AbstractHttpSessionApplicationInitializer {
             public List<String> readCookieValues(HttpServletRequest request) {
                 Cookie[] cookies = request.getCookies();
                 if (cookies != null) {
-                    LOG.info("readCookieValues with cookies "+ Stream.of(cookies).map(cookie -> cookie.getValue()).collect(Collectors.toList()));
+                    LOG.info("readCookieValues with cookies "+ Stream.of(cookies).map(cookie ->
+                            String.format("Name %s, value %s",cookie.getName(), cookie.getValue())).collect(Collectors.toList()));
                 } else {
                     LOG.info("readCookieValues with null cookies ");
                 }
-                List<String> cookieValues = super.readCookieValues(request);
-                LOG.debug("readCookieValues: " + cookieValues);
-                return cookieValues;
+                return super.readCookieValues(request);
             }
 
             @Override
             public void writeCookieValue(CookieValue cookieValue) {
-                LOG.info("writeCookieValue: " + (cookieValue != null ? cookieValue.getCookieValue() : "null"));
                 super.writeCookieValue(cookieValue);
+                Collection<String> headers = cookieValue.getResponse().getHeaders("Set-Cookie");
+                LOG.info("WriteCookieValue: " + headers);
+
             }
         };
         //We don't need same-site as the load-balancer takes care of this
