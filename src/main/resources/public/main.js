fb3145224e213ffd95d5f51b060cb2e03b4b12f0
Okke Harsta <oharsta@zilverline.com>
2024-08-16 16:22:56 +0200

    Removed console.log statements

--- src/main/resources/public/main.js
+++ src/main/resources/public/main.js
@@ -2,9 +2,6 @@ document.addEventListener("DOMContentLoaded", function () {
     var inputField = document.getElementById("userCode");
     inputField.oninput = function (e) {
         var value = e.target.value;
-        console.log(e.inputType);
-        console.log(e.target.value);
-        console.log(e.target.length);
         var newValue = this.value.toUpperCase();
         if (value.length === 4 && e.inputType === "insertText" && !value.endsWith("-")) {
             newValue += "-";
