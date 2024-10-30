b12d13bae4418b83013ff0a3ba5b835dcd1f8073
Tim van Dijen <tvdijen@gmail.com>
2024-10-30 16:41:23 +0100

    Use legacy JS instead of event handlers
    Legacy browsers and 'embedded' browsers like the one in Office do not always support event handlers

--- src/main/resources/public/post.js
+++ src/main/resources/public/post.js
@@ -1,3 +1,6 @@
-document.addEventListener("DOMContentLoaded", function () {
+window.handleState = function handleState() {
+  if (document.readyState === 'interactive' || document.readyState === "complete") {
     document.forms[0].submit();
-});
+  }
+};
+document.onreadystatechange = window.handleState;
