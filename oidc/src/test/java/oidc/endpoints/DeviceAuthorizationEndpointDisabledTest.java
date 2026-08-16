41f976959ea97f52e7ffd4434815507f2b04cbbd
Okke Harsta <oharsta@gmail.com>
2026-08-16 08:59:45 +0200

    Fixed test after 437e1ac4047df075f2c27fa2017d1c14b962ea34

--- oidc/src/test/java/oidc/endpoints/DeviceAuthorizationEndpointDisabledTest.java
+++ oidc/src/test/java/oidc/endpoints/DeviceAuthorizationEndpointDisabledTest.java
@@ -6,30 +6,27 @@ import oidc.AbstractIntegrationTest;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Map;
-
 import static io.restassured.RestAssured.given;
-import static org.junit.Assert.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
-        properties = {
-                "cron.node-cron-job-responsible=false",
-                "features.oidcng_device_flow=false"
-        })
+    properties = {
+        "cron.node-cron-job-responsible=false",
+        "features.oidcng_device_flow=false"
+    })
 public class DeviceAuthorizationEndpointDisabledTest extends AbstractIntegrationTest {
 
     @SneakyThrows
     @Test
     public void deviceAuthorizationHappyFlow() {
-        Map<String, Object> body = given()
-                .when()
-                .header("Content-type", "application/x-www-form-urlencoded")
-                .formParam("grant_type", GrantType.AUTHORIZATION_CODE.getValue())
-                .formParam("client_id", "mock-sp")
-                .formParam("scope", "openid groups")
-                .post("oidc/device_authorization")
-                .as(mapTypeRef);
-        assertEquals(404, body.get("status"));
+        given()
+            .when()
+            .header("Content-type", "application/x-www-form-urlencoded")
+            .formParam("grant_type", GrantType.AUTHORIZATION_CODE.getValue())
+            .formParam("client_id", "mock-sp")
+            .formParam("scope", "openid groups")
+            .post("oidc/device_authorization")
+            .then()
+            .statusCode(404);
 
     }
 
